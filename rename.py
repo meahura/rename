eb11f63af616be523287383477201c7645728ecb
AhSiber <Ahurarasoli@gmail.com>
2022-05-27 21:26:03 +0430

    add!,random color and faster

--- rename.py
+++ rename.py
@@ -1,35 +1,36 @@
-import sys 
+import sys
 import os
-import platform 
-from colorama import Fore
+import platform
+from ColorRandom.color_random_txt import colorMeakUP
 
-def main(platfroms:str): 
-   if platform == platfroms: 
-      try: 
+def main(platfroms:str):
+   if platform == platfroms:
+      try:
          print("wait install...")
-         os.system("bash AhutoInstall.sh") 
-      except: 
-         pass 
+         os.system("bash AhutoInstall.sh")
+      except:
+         pass
    else:
-      pass 
-   
-   try: 
+      pass
 
+   try:
+      
       args = sys.argv[1:100]
       sort_args =  " ".join(args)
 
-      if sort_args == "-h" or sort_args == "--help": 
-         print(f""" 
+      if sort_args == "-h" or sort_args == "--help":
+         colorMeakUP.color.Color("welocome to tools,rename! command down : \nrename [New Name] [old file]")
 
-welocome to tools,rename! command down;
-rename [New Name] [old file]""")
-      else: 
-         with open(args[1],mode='r') as readname: 
+      elif sort_args == "-o" or sort_args == '--owner':
+         colorMeakUP.color.Color("http://github.com/AhSiber")
+
+      else:
+         with open(args[1],mode='r') as readname:
             read_file = readname.read()
 
-         with open(args[0] , mode='w') as w: 
+         with open(args[0] , mode='w') as w:
             os.remove(args[1])
-            w.write(read_file) 
-   except: 
-      pass  
-main("linux".lower()) 
+            w.write(read_file)
+   except:
+      pass
+main("linux".lower())
